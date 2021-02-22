236202b87f3a9c9dcca3fa6a53541b772b38461a
Robin <robin.kim@coinbase.com>
2021-02-21 23:57:10 -0500

    zoom and fullscreen fx

--- static/script.js
+++ static/script.js
@@ -1,3 +1,17 @@
+let isFullscreen = false;
+
+function registerFullscreenHandler() {
+  document.addEventListener("fullscreenchange", fullscreenHandler, false);
+}
+
+function fullscreenHandler() {
+  isFullscreen = document.webkitIsFullScreen;
+  if (isFullscreen) $("#image-root").css({ cursor: "zoom-out" });
+  else $("#image-root").css({ cursor: "zoom-in" });
+}
+
+registerFullscreenHandler();
+
 async function getAImages() {
   const response = await fetch("/getImages");
   const data = await response.json();
@@ -11,37 +25,57 @@ $("#fetch-button").click(async () => {
 
   $("#fetch-button").html("SUCCESS!");
   $("#fetch-button").prop("disabled", true);
-  $("#random-pic-button").prop("disabled", false);
+  $("#start-button").prop("disabled", false);
 
   localStorage.setItem("imageUrls", JSON.stringify(data));
 });
 
-let used = {};
+const goFullscreen = () => $("#image-root")[0].requestFullscreen();
 
-const MAX_DIMENSION = 800; // this should scale to viewport
-
-$("#random-pic-button").click(() => {
-  const urlString = localStorage.getItem("imageUrls");
-  const urls = JSON.parse(urlString);
-
-  let url = getRandomEl(urls);
-  if (used[url]) url = getRandomEl(urls);
-  used[url] = true;
+$("#start-button").click(() => {
+  const url = getUrlFromLocalStorage();
 
   const img = new Image();
   img.src = url;
   img.onload = () => {
-    let w = img.width * 2;
-    let h = img.height * 2;
-    if (h > MAX_DIMENSION) {
-      let ratio = MAX_DIMENSION / h;
-      h = MAX_DIMENSION;
-      w = w * ratio;
-    }
-    $("#image-root").attr("src", url).width(w).height(h);
+    goFullscreen();
+    $("#image-root").attr("src", url);
+    document.body.onkeyup = registerKeyboardEvents;
   };
+});
+
+$("#image-root").click(() => {
+  if (!isFullscreen) goFullscreen();
+  else document.exitFullscreen();
 });
 
 function getRandomEl(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
+}
+
+const used = {};
+function getRandomUnusedUrl(urls) {
+  const url = getRandomEl(urls);
+  if (used[url]) return getRandomUnusedUrl(urls);
+  used[url] = true;
+  return url;
+}
+
+function getUrlFromLocalStorage() {
+  const urlString = localStorage.getItem("imageUrls");
+  const urls = JSON.parse(urlString);
+  return getRandomUnusedUrl(urls);
+}
+
+function registerKeyboardEvents(event) {
+  const nextKeyMap = {
+    space: 32,
+    arrowRight: 39,
+  };
+  const nextKeys = Object.values(nextKeyMap);
+
+  if (nextKeys.includes(event.keyCode)) {
+    const newUrl = getUrlFromLocalStorage();
+    $("#image-root").attr("src", newUrl);
+  }
 }
